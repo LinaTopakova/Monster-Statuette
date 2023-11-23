314e4feccd226d1017562c4a8da84b345172aafd
Udynix <129510560+LinaTopakova@users.noreply.github.com>
2023-11-23 11:07:28 +0700

    New commit

--- Assets/Code/walk.cs
+++ Assets/Code/walk.cs
@@ -1,26 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
-public class walking : MonoBehaviour
+public class walk : MonoBehaviour
 {
-    public float speed = 2f;
-    public Animator animator;
+    private float speed = 1;
     private Vector2 direction;
     private Rigidbody2D rb;
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
+    // Update is called once per frame
     void Update()
     {
         direction.x = Input.GetAxisRaw("Horizontal");
         direction.y = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Horizontal", direction.x);
-        animator.SetFloat("Vertical", direction.y);
-        animator.SetFloat("Speed", direction.sqrMagnitude);
     }
 
     private void FixedUpdate()
