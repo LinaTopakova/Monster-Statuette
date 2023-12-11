29574108e5f20529c701e3033b0af74875f4a846
Udynix <129510560+LinaTopakova@users.noreply.github.com>
2023-12-11 23:53:33 +0700

    Scene

--- Assets/Code/sort.cs
+++ Assets/Code/sort.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class sort : MonoBehaviour
 {
     public Vector3 V3;
-    private Renderer r;
+    public Renderer r;
     void Start()
     {
         r = GetComponent<Renderer>();
@@ -16,23 +16,25 @@ public class sort : MonoBehaviour
         V3 = this.transform.position;
         if ((V3.y<0.796 && V3.y>0.615)&&(V3.x<1.11 && V3.x > 0.561))
         {
+            //Debug.Log("1");
             r.sortingOrder = 1;
         }
         else if((V3.y < 0.796 && V3.y > 0.615) && (V3.x < -0.613 && V3.x > -1.05))
         {
             r.sortingOrder = 1;
+            Debug.Log(r.sortingOrder);
         }
         else if ((V3.y < -0.615 && V3.y > -0.796) && (V3.x < -0.613 && V3.x > -1.05))
         {
-            r.sortingOrder = 3;
+            r.sortingOrder = 5;
         }
         else if ((V3.y < -0.615 && V3.y > -0.796) && (V3.x < 1.11 && V3.x > 0.561))
         {
-            r.sortingOrder = 3;
+            r.sortingOrder = 5;
         }
         else
         {
-            r.sortingOrder = 2;
+            r.sortingOrder = 3;
         }
     }
 }
